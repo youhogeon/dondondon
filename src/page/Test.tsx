d31d75cf51f950c966d27194a2da51703de24763
YouHoGeon <me@youhogeon.com>
2023-07-18 22:33:24 +0900

    add: sample 소득 테이블

--- src/page/Test.tsx
+++ src/page/Test.tsx
@@ -1,3 +1,4 @@
+import { useEffect, useRef } from 'react';
 import { Button, CardActions, CardContent, CardHeader, Stack } from '@mui/material'
 import Card from '../common/Card'
 
@@ -5,47 +6,84 @@ import { HotTable } from '@handsontable/react';
 import { registerAllModules } from 'handsontable/registry';
 
 import 'handsontable/dist/handsontable.full.min.css';
-import { useEffect, useRef } from 'react';
+import { DetailedSettings } from 'handsontable/plugins/nestedHeaders';
+import { ColumnSettings } from 'handsontable/settings';
 
 registerAllModules();
 
 const Test = () => {
-    const hotRef = useRef();
+    const hotRef = useRef<HotTable>(new HotTable({}));
 
     useEffect(() => {
-        // const hot = hotRef.current.hotInstance;
+        const hot = hotRef.current?.hotInstance;
 
+        if (!hot) return;
 
+        hot.updateSettings({
+            cells(row, col) {
+                const cellProperties: {
+                    readOnly?: boolean;
+                } = {};
+
+                if (row === 12 || col === 13) {
+                    cellProperties.readOnly = true;
+                }
+
+                return cellProperties;
+            }
+        })
     })
+
+    const onClick = () => {
+        console.log(data)
+    }
 
     const rowHeaders = [ ...Array(12).keys() ].map((i) => `${i + 1}월`)
     rowHeaders.push('합계')
 
-    const colHeaders = [
+    const columns = [
+        '급여',
+        '정기상여',
+        '상여금',
+        '기타급여',
+        '특허',
+        '기타 (중식비 등)',
+        '소득세',
+        '지방소득세',
+        '국민건강보험',
+        '장기요양보험',
+        '국민연금',
+        '고용보험료',
+        '기타공제',
+        '실질 수령액'
+    ]
+
+    const columns_info: ColumnSettings[] = []
+    columns.forEach(column => {
+        columns_info.push({
+            data: column,
+            type: 'numeric',
+            numericFormat: {
+                pattern: '0,0',
+            }
+        })
+    })
+
+    const colHeaders: (string | DetailedSettings)[][] = [
         [
             { label: '세전 수입 (비과세 포함)', colspan: 4 },
             { label: '수입 중 비과세', colspan: 2 },
             { label: '원천징수', colspan: 2 },
             { label: '공제(4대보험 등)', colspan: 5 },
-            { label: '합계' }
+            { label: '합계', colspan: 1 }
         ],
-        [
-            '급여',
-            '정기상여',
-            '상여금',
-            '기타급여',
-            '특허',
-            '기타 (중식비 등)',
-            '소득세',
-            '지방소득세',
-            '국민건강보험',
-            '장기요양보험',
-            '국민연금',
-            '고용보험료',
-            '기타공제',
-            '실질 수령액'
-        ]
+        [...columns]
     ]
+
+    const data: object[] = []
+    for (let i = 0; i < rowHeaders.length; i++) {
+        data.push({})
+    }
 
     return (
         <>
@@ -55,25 +93,18 @@ const Test = () => {
                     <CardContent>
                         <HotTable
                             ref={hotRef}
-                            startRows={13}
-                            startCols={14}
                             rowHeaders={rowHeaders}
                             colHeaders={true}
                             nestedHeaders={colHeaders}
-                            type='numeric'
-
+                            columns={columns_info}
+                            data={data}
                             height='auto'
-                            width='100%'
+
                             licenseKey='non-commercial-and-evaluation'
-                            afterChange={function (change: string, source: string) {
-                                // eslint-disable-next-line no-console
-                                console.log(change, source)
-                            }}
                         />
                     </CardContent>
                     <CardActions>
-                        <Button size='small'>Share</Button>
-                        <Button size='small'>Learn More</Button>
+                        <Button size='small' onClick={onClick}>로그 찍기</Button>
                     </CardActions>
                 </Card>
             </Stack>
--- src/vite-env.d.ts
+++ src/vite-env.d.ts
@@ -1,8 +1,5 @@
 /// <reference types='vite/client' />
 
-declare module '@handsontable/react'
-declare module 'handsontable/registry'
-
 interface ImportMeta {
     env: {
         APP_TITLE: string,
