a524b8c9157b52c453f51c1ba46e40612f73eac9
YouHoGeon <me@youhogeon.com>
2023-07-20 23:46:27 +0900

    add: HandsonTable (HotTable Wrapper)

---
+++ src/common/HandsonTable.tsx
@@ -0,0 +1,125 @@
+import HotTable, { HotTableProps } from '@handsontable/react'
+import { registerAllModules } from 'handsontable/registry';
+
+import 'handsontable/dist/handsontable.full.min.css';
+import { createRef, forwardRef, useImperativeHandle, useMemo, useRef } from 'react';
+import { DetailedSettings } from 'handsontable/plugins/nestedHeaders';
+import { ColumnSettings } from 'handsontable/settings';
+
+registerAllModules();
+
+interface NestedColumns {
+    name: string;
+    children?: string[];
+}
+
+interface HandsonTableProps {
+    columns: string[] | NestedColumns[],
+    rows: string[];
+
+    columnsInfo?: ColumnSettings[];
+}
+
+const convertNestedColumns = (columns: NestedColumns[]): [DetailedSettings[], string[]] => {
+    const superColumns: DetailedSettings[] = []
+    const subColumns: string[] = []
+
+    columns.forEach(column => {
+        if (!column.children) {
+            superColumns.push({
+                label: '',
+                colspan: 1
+            });
+
+            subColumns.push(column.name)
+
+            return
+        }
+
+        superColumns.push({
+            label: column.name,
+            colspan: column.children.length
+        })
+
+        subColumns.push(...column.children)
+    });
+
+    return [superColumns, subColumns]
+}
+
+const convertColumnsInfo = (columns: string[]): ColumnSettings[] => {
+    const columnsInfo: ColumnSettings[] = []
+    columns.forEach(column => {
+        columnsInfo.push({
+            data: column,
+            type: 'numeric',
+            numericFormat: {
+                pattern: '0,0',
+            }
+        })
+    })
+    
+    return columnsInfo
+}
+
+interface HandsonTableRef {
+    data: object[];
+    hot: HotTable | null;
+}
+
+const HandsonTable = (props: HandsonTableProps, ref: React.Ref<HandsonTableRef>) => {
+    const hotRef = createRef<HotTable>()
+
+    const initialData = useMemo(() => {
+        const data: object[] = []
+
+        for (let i = 0; i < props.rows.length; i++) {
+            data.push({})
+        }
+
+        return data
+    }, [props.rows])
+
+    const data = useRef(initialData)
+
+    useImperativeHandle(ref, () => ({
+        get data() {
+            return data.current;
+        },
+        get hot() {
+            return hotRef.current;
+        }
+    }))
+
+    const settings: HotTableProps = {
+        rowHeaders: props.rows,
+        data: data.current,
+        height: 'auto',
+        licenseKey: 'non-commercial-and-evaluation'
+    }
+
+    const columns: string[] = []
+
+    if (props.columns.length && typeof props.columns[0] === 'object') { //for nested headers
+        const converted = convertNestedColumns(props.columns as NestedColumns[])
+
+        settings.nestedHeaders = converted
+        settings.colHeaders = true
+        columns.push(...converted[1])
+    } else {
+        settings.colHeaders = props.columns as string[]
+        columns.push(...settings.colHeaders)
+    }
+
+    settings.columns = props.columnsInfo || convertColumnsInfo(columns)
+
+    return (
+        <HotTable
+            ref={hotRef}
+            {...settings}
+        ></HotTable>
+    )
+}
+
+export type { HandsonTableRef }
+export default forwardRef(HandsonTable)
--- src/page/Test.tsx
+++ src/page/Test.tsx
@@ -1,88 +1,50 @@
-import { useEffect, useRef } from 'react';
 import { Button, CardActions, CardContent, CardHeader, Stack } from '@mui/material'
 import Card from '../common/Card'
 
-import { HotTable } from '@handsontable/react';
-import { registerAllModules } from 'handsontable/registry';
+import HandsonTable, { HandsonTableRef } from '../common/HandsonTable';
+import { createRef } from 'react';
 
-import 'handsontable/dist/handsontable.full.min.css';
-import { DetailedSettings } from 'handsontable/plugins/nestedHeaders';
-import { ColumnSettings } from 'handsontable/settings';
+const rows = [...Array(12).keys()].map((i) => `${i + 1}월`)
+rows.push('합계')
 
-registerAllModules();
+const columns = [
+    {
+        name: '세전 수입 (비과세 포함)',
+        children: [
+            '급여', '정기상여', '상여금', '기타급여'
+        ]
+    },
+    {
+        name: '수입 중 비과세',
+        children: [
+            '특허', '기타 (중식비 등)'
+        ]
+    },
+    {
+        name: '원천징수',
+        children: [
+            '소득세', '지방소득세'
+        ]
+    },
+    {
+        name: '공제(4대보험 등)',
+        children: [
+            '국민건강보험', '장기요양보험', '국민연금', '고용보험료', '기타공제'
+        ]
+    },
+    {
+        name: '합계',
+        children: [
+            '실질 수령액'
+        ]
+    }
+]
 
 const Test = () => {
-    const hotRef = useRef<HotTable>(new HotTable({}));
-
-    useEffect(() => {
-        const hot = hotRef.current?.hotInstance;
-
-        if (!hot) return;
-
-        hot.updateSettings({
-            cells(row, col) {
-                const cellProperties: {
-                    readOnly?: boolean;
-                } = {};
-
-                if (row === 12 || col === 13) {
-                    cellProperties.readOnly = true;
-                }
-
-                return cellProperties;
-            }
-        })
-    })
+    const hotRef = createRef<HandsonTableRef>()
 
     const onClick = () => {
-        console.log(data)
-    }
-
-    const rowHeaders = [ ...Array(12).keys() ].map((i) => `${i + 1}월`)
-    rowHeaders.push('합계')
-
-    const columns = [
-        '급여',
-        '정기상여',
-        '상여금',
-        '기타급여',
-        '특허',
-        '기타 (중식비 등)',
-        '소득세',
-        '지방소득세',
-        '국민건강보험',
-        '장기요양보험',
-        '국민연금',
-        '고용보험료',
-        '기타공제',
-        '실질 수령액'
-    ]
-
-    const columns_info: ColumnSettings[] = []
-    columns.forEach(column => {
-        columns_info.push({
-            data: column,
-            type: 'numeric',
-            numericFormat: {
-                pattern: '0,0',
-            }
-        })
-    })
-
-    const colHeaders: (string | DetailedSettings)[][] = [
-        [
-            { label: '세전 수입 (비과세 포함)', colspan: 4 },
-            { label: '수입 중 비과세', colspan: 2 },
-            { label: '원천징수', colspan: 2 },
-            { label: '공제(4대보험 등)', colspan: 5 },
-            { label: '합계', colspan: 1 }
-        ],
-        [...columns]
-    ]
-
-    const data: object[] = []
-    for (let i = 0; i < rowHeaders.length; i++) {
-        data.push({})
+        alert(JSON.stringify(hotRef.current?.data))
     }
 
     return (
@@ -91,16 +53,10 @@ const Test = () => {
                 <Card>
                     <CardHeader title='소득 정보 입력' />
                     <CardContent>
-                        <HotTable
+                        <HandsonTable
                             ref={hotRef}
-                            rowHeaders={rowHeaders}
-                            colHeaders={true}
-                            nestedHeaders={colHeaders}
-                            columns={columns_info}
-                            data={data}
-                            height='auto'
-
-                            licenseKey='non-commercial-and-evaluation'
+                            columns={columns}
+                            rows={rows}
                         />
                     </CardContent>
                     <CardActions>
